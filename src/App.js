31881f64e4546cd30a3dd2a1e0b34398c0c7f73a
Faisallm <faisallawan1997@gmail.com>
2023-09-16 15:05:20 -0700

    gear 1st

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar/Navbar";
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+// importing the css files
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import contactImg from '../../assets/contact.png';
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="Logo" className="logo" />
+
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+      </div>
+
+      <button className="desktopMenuBtn">
+      
+        <img src={contactImg} className="desktopMenuImg" />
+        Contact Me
+      
+      </button>
+
+    </nav>
+  );
+};
+
+export default Navbar;
