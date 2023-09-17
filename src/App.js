93f25274302ad38ef279da690a5ecb325f44c694
Faisallm <faisallawan1997@gmail.com>
2023-09-17 05:53:15 -0700

    gear 2nd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Skills />
     </div>
   );
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+// import the required classes
+import "./hero.css";
+import bg from "../../assets/image.png";
+import { Link } from "react-scroll";
+import btnImg from "../../assets/hireme.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section id="hero">
+      <div className="heroContent">
+        <span className="hello">Hello,</span>
+
+        <span className="heroText">
+          I'm <span className="heroName">Smith</span>
+          <br />
+          Website Designer
+        </span>
+
+        <p className="heroPara">
+          I am a skilled and passionate web designer with experience in creating
+          visually <br /> appealing and user-friendly websites.
+        </p>
+
+        <Link>
+          <button className="btn">
+            <img src={btnImg} alt="Hire me" />
+            Hire me
+          </button>
+        </Link>
+      </div>
+      <img src={bg} className="bg" />
+    </section>
+  );
+};
 
 export default Hero;
