b9fcc23b30dde413cbf994bfa1edbeb64a64a93f
Faisallm <faisallawan1997@gmail.com>
2023-09-18 19:42:43 -0700

    Added footer

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,144 @@
-import React from "react";
+import React, { useState } from "react";
 // importing the css files
 import "./navbar.css";
 import logo from "../../assets/logo.png";
-import contactImg from '../../assets/contact.png';
+import contactImg from "../../assets/contact.png";
 import { Link } from "react-scroll";
+import menu from "../../assets/menu.png";
 
 const Navbar = () => {
+  // at first showMenu is false
+  //   react hook
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
 
       <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About</Link>
-        <Link className="desktopMenuListItem">Portfolio</Link>
-        <Link className="desktopMenuListItem">Clients</Link>
+        <Link
+          activeClass="active"
+          to="hero"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Clients
+        </Link>
       </div>
 
-      <button className="desktopMenuBtn">
-      
+      <button
+        className="desktopMenuBtn"
+        onClick={() =>
+          document
+            .getElementById("contact")
+            .scrollIntoView({ behavior: "smooth" })
+        }
+      >
         <img src={contactImg} className="desktopMenuImg" />
         Contact Me
-      
       </button>
 
+      {/* once i click image
+        i want the whole thing to appear */}
+      <img src={menu} className="mobMenu" onClick={() => setShowMenu(!showMenu)} />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="hero"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Clients
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
